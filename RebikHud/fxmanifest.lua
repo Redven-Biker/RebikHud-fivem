f00a0f4e21f3a8a60abcddb7c32da530f4856b65
Redven-Biker <marween35620@gmail.com>
2021-08-22 07:59:17 +0200

    1.0

---
+++ RebikHud/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+    "ui.html",
+    "assets/css/ui.css",
+    "assets/js/ui.js",
+	'assets/img/armor.png',
+	'assets/img/health.png',
+	'assets/img/hunger.png',
+	'assets/img/speaker.png',
+	'assets/img/stamina.png',
+	'assets/img/water.png',
+}
+
+client_script {
+    'client.lua'
+}
+
+ui_page "ui.html"
